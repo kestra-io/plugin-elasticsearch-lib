6428b8072dfcac373ec1d04940056f2ad65f3f58
jymaire <jmaire@kestra.io>
2026-09-08 16:49:41 +0200

    chore: initial scaffold cleanup for plugin-elasticsearch-lib

---
+++ src/main/java/io/kestra/plugin/elasticsearch-lib/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Elasticsearch Lib",
+    description = "Elasticsearch Lib plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.elasticsearch-lib;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
